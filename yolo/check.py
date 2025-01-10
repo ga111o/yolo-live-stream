11ac99ada9e16bc64fb7d945b40ae0f45045a79b
ga111o <ga111o@proton.me>
2025-01-10 19:20:27 +0900

    속도측정

--- yolo/check.py
+++ yolo/check.py
@@ -2,35 +2,39 @@ import torch
 from ultralytics import YOLO
 import os
 from PIL import Image
+import time
+from icecream import ic
 
 # 학습된 모델 로드
 model = YOLO('./trained_model_20250109_182911.pt')
 
-# train_images 디렉토리의 모든 이미지 처리
-image_dir = './yolo/train_images'
-image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
+# 예측 시간 측정 시작
+start_time = time.time()
 
-# 모든 이미지에 대해 반복
-for image_file in image_files:
-    image_path = os.path.join(image_dir, image_file)
+# 전체 디렉토리에 대한 예측 수행
+results = model.predict(source='./yolo/test_images', save=True, conf=0.25)
+
+# 예측 시간 측정 종료
+end_time = time.time()
+prediction_time_ms = (end_time - start_time) * 1000
+prediction_time_ms = f"{prediction_time_ms:.3f}"
+
+# 전체 예측 시간 출력
+ic(prediction_time_ms)
+
+# 결과 처리
+for r in results:
+    # 이미지 파일 이름 출력
+    ic(os.path.basename(r.path))
+    boxes = r.boxes
+    for box in boxes:
+        # 바운딩 박스 좌표
+        x1, y1, x2, y2 = box.xyxy[0]
+        # 신뢰도
+        confidence = box.conf[0]
+        # 클래스
+        cls = box.cls[0]
+        class_name = model.names[int(cls)]
+        
+        ic(class_name, confidence)
     
-    # 이미지 로드 및 예측
-    results = model.predict(image_path, save=True, conf=0.25)  # conf는 confidence threshold
-    
-    # 결과 출력
-    # print(f"\n=================\n처리중인 이미지: {image_file}")
-    # print("예측 결과:")
-    # for r in results:
-    #     boxes = r.boxes
-    #     for box in boxes:
-    #         # 바운딩 박스 좌표
-    #         x1, y1, x2, y2 = box.xyxy[0]
-    #         # 신뢰도
-    #         confidence = box.conf[0]
-    #         # 클래스
-    #         cls = box.cls[0]
-    #         class_name = model.names[int(cls)]
-            
-    #         print(f"클래스: {class_name}, 신뢰도: {confidence:.2f}")
-    # print("=================")
-    # print("=================\n")
