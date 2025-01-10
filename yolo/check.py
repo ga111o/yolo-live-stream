30392441f11f593fe5d061fc5b9c1e3d32abd6a0
ga111o <ga111o@proton.me>
2025-01-10 20:08:12 +0900

    path에대해서수행할까하다가.. 시간 재는 방법 맞춰야 할 거 같아서 각 이미지에 대해서 -

--- yolo/check.py
+++ yolo/check.py
@@ -8,33 +8,34 @@ from icecream import ic
 # 학습된 모델 로드
 model = YOLO('./trained_model_20250109_182911.pt')
 
-# 예측 시간 측정 시작
-start_time = time.time()
+# train_images 디렉토리의 모든 이미지 처리
+image_dir = './yolo/test_images'
+image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
-# 전체 디렉토리에 대한 예측 수행
-results = model.predict(source='./yolo/test_images', save=True, conf=0.25)
-
-# 예측 시간 측정 종료
-end_time = time.time()
-prediction_time_ms = (end_time - start_time) * 1000
-prediction_time_ms = f"{prediction_time_ms:.3f}"
-
-# 전체 예측 시간 출력
-ic(prediction_time_ms)
-
-# 결과 처리
-for r in results:
-    # 이미지 파일 이름 출력
-    ic(os.path.basename(r.path))
-    boxes = r.boxes
-    for box in boxes:
-        # 바운딩 박스 좌표
-        x1, y1, x2, y2 = box.xyxy[0]
-        # 신뢰도
-        confidence = box.conf[0]
-        # 클래스
-        cls = box.cls[0]
-        class_name = model.names[int(cls)]
-        
-        ic(class_name, confidence)
+# 모든 이미지에 대해 반복
+for image_file in image_files:
+    image_path = os.path.join(image_dir, image_file)
+    
+    start_time = time.time()
+    
+    results = model.predict(image_path)
+    
+    end_time = time.time()
+    prediction_time_ms = (end_time - start_time) * 1000
+    prediction_time_ms = f"{prediction_time_ms:.3f}"
+    
+    # 결과 출력
+    ic(image_file, prediction_time_ms)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # 바운딩 박스 좌표
+            x1, y1, x2, y2 = box.xyxy[0]
+            # 신뢰도
+            confidence = box.conf[0]
+            # 클래스
+            cls = box.cls[0]
+            class_name = model.names[int(cls)]
+            
+            ic(class_name, confidence)
     
